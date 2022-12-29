0d4649b22d0cfff373a6708014eb2563b6eb6f5b
DavidFantin <davidfantin.2000@gmail.com>
2022-12-29 01:47:20 -0600

    initial commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+This project was inspired by:
+
+https://lifehacker.com/this-auto-trigger-spray-bottle-pranks-friends-keeps-ca-1555621712
+
+Parts List:
+- NPN transistor (x1)
+- Diode (x1)
+- Humidity sensor [] (x1)
+- Arduino Nano (ATmega328) (x1)
+- Sprayer portion of a raid auto trigger sprayer (with 3v DC motor)
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+/*
+* Hardware: Arduino Nano (ATmega328)
+* Author: David Fantin
+* Date created: 12/28/2022
+* Last update: 12/28/2022
+*/
+
+#include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
+#define SPRAY 12
+
+DHT dht(DHTPIN, DHTTYPE);
+double idealHumi = 40;
+
+void setup() {
+   Serial.begin(9600);
+   pinMode(SPRAY, OUTPUT);
+   dht.begin();
+}
+
+void loop() {
+   // delay between measurments
+   delay(2000);
+
+   // read humidity
+   float humi  = dht.readHumidity();
+
+   // check if any read failed
+   if (isnan(humi)) {
+      Serial.println("Failed to read from DHT sensor!");
+   } else {
+      Serial.print("Humidity: ");
+      Serial.print(humi);
+      Serial.print("%");
+
+      // if humidity is under the ideal percentage, start the spray sequence
+      if (humi < idealHumi) {
+         // spray sequence
+         digitalWrite(SPRAY, HIGH);
+         delay(10000);
+         digitalWrite(SPRAY, LOW);
+      }
+   }
+}
